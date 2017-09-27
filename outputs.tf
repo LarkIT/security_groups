92ef4fcdbcfd3fdae5647041c2a57f4573171a8f
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-09-27 08:20:11 -0600

    output for ssh_jump

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "general_id" {
   value = "${aws_security_group.general.id}"
 }
+
+output "ssh_jump_id" {
+  value = "${aws_security_group.ssh_jump.id}"
+}
+
+output "group_id" {
+  value = "${aws_security_group.general.id}"
+}
