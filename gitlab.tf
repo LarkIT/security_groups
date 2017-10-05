72f623b79f90c5ed1f3ee2dcc48f5b1aaaad351f
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-05 14:55:04 -0600

    minor

---
+++ gitlab.tf
@@ -0,0 +1,74 @@
+resource "aws_security_group" "gitlab" {
+  name        = "gitlab"
+  description = "Allow  GitLab connections."
+  vpc_id      = "${var.vpc_id}"
+  tags { Name = "gitlab" }
+}
+
+resource "aws_security_group_rule" "gitlab-in-any-http" {
+  security_group_id = "${aws_security_group.gitlab.id}"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr}"] # TEMPORARY - NEEDS SECURED
+}
+
+resource "aws_security_group_rule" "gitlab-in-any-https" {
+  security_group_id = "${aws_security_group.gitlab.id}"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr}"] # TEMPORARY - NEEDS SECURED
+}
+
+
+resource "aws_security_group_rule" "gitlab-out-any" {
+  security_group_id = "${aws_security_group.gitlab.id}"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["${var.cidr}"] # TEMPORARY - NEEDS SECURED
+}
+
+resource "aws_security_group_rule" "gitlab-in-foreman-ssh" {
+  security_group_id        = "${aws_security_group.gitlab.id}"
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.foreman.id}"
+}
+
+resource "aws_security_group_rule" "gitlab-out-foreman-webhook" {
+  security_group_id        = "${aws_security_group.gitlab.id}"
+  type                     = "egress"
+  from_port                = 8088
+  to_port                  = 8088
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.foreman.id}"
+}
+
+### IF INITIAL STATE
+
+resource "aws_security_group_rule" "gitlab-out-any-http" {
+  security_group_id = "${aws_security_group.gitlab.id}"
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "gitlab-out-any-https" {
+  security_group_id = "${aws_security_group.gitlab.id}"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+### END IF INITIAL STATE
