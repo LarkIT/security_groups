7b18cdc05be9a292819cb6267b581e5617647ac8
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-11 23:07:35 -0700

    minor

--- proddb.tf
+++ proddb.tf
@@ -1,16 +1,16 @@
-resource "aws_security_group" "proddb" {
-    name = "proddb"
-    description = "Allow proddb connections."
-    tags { Name = "proddb" }
-    vpc_id = "${var.vpc_id}"
-}
+#resource "aws_security_group" "proddb" {
+#    name = "proddb"
+#    description = "Allow proddb connections."
+#    tags { Name = "proddb" }
+#    vpc_id = "${var.vpc_id}"
+#}
 
-resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
-  security_group_id        = "${aws_security_group.proddb.id}"
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
+#resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
+#  security_group_id        = "${aws_security_group.proddb.id}"
+#  type                     = "ingress"
+#  from_port                = 5432
+#  to_port                  = 5432
+#  protocol                 = "tcp"
 #  source_security_group_id = "${aws_security_group.prodapp.id}"
-}
+#}
 
