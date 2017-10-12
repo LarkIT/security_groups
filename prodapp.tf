0cd1ee76c8699333c34896d14d4ef1576af91b5b
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-11 23:34:14 -0700

    minor

--- prodapp.tf
+++ prodapp.tf
@@ -87,14 +87,14 @@ resource "aws_security_group_rule" "prodapp-in-jumphost-https" {
   source_security_group_id = "${aws_security_group.ssh_jump.id}"
 }
 
-resource "aws_security_group_rule" "prodapp-out-proddb-sql" {
-  security_group_id = "${aws_security_group.prodapp.id}"
-  type = "egress"
-  from_port = 1433
-  to_port = 1433
-  protocol = "tcp"
-  source_security_group_id = "${aws_security_group.proddb.id}"
-}
+#resource "aws_security_group_rule" "prodapp-out-proddb-sql" {
+#  security_group_id = "${aws_security_group.prodapp.id}"
+#  type = "egress"
+#  from_port = 1433
+#  to_port = 1433
+#  protocol = "tcp"
+#  source_security_group_id = "${aws_security_group.proddb.id}"
+#}
 
 resource "aws_security_group_rule" "prodapp-out-proxy" {
   security_group_id = "${aws_security_group.prodapp.id}"
