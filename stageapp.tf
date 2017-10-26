b9052aa4827bfdf2cd0291a6697c0e2befc7653c
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-26 13:09:29 -0600

    minor

--- stageapp.tf
+++ stageapp.tf
@@ -60,24 +60,6 @@ resource "aws_security_group_rule" "stageapp-in-vpn-https" {
   source_security_group_id = "${aws_security_group.vpn.id}"
 }
 
-#resource "aws_security_group_rule" "stageapp-in-vpn-vhttps" {
-#  security_group_id = "${aws_security_group.stageapp.id}"
-#  type = "ingress"
-#  from_port = 2001
-#  to_port = 2010
-#  protocol = "tcp"
-#  source_security_group_id = "${aws_security_group.stageapp-lb.id}"
-#}
-
-#resource "aws_security_group_rule" "stageapp-in-jumphost-rdp" {
-#  security_group_id = "${aws_security_group.stageapp.id}"
-#  type = "ingress"
-#  from_port = 3389
-#  to_port = 3389
-#  protocol = "tcp"
-#  source_security_group_id = "${aws_security_group.ssh_jump.id}"
-#}
-
 resource "aws_security_group_rule" "stageapp-in-jumphost-https" {
   security_group_id = "${aws_security_group.stageapp.id}"
   type = "ingress"
@@ -86,15 +68,6 @@ resource "aws_security_group_rule" "stageapp-in-jumphost-https" {
   protocol = "tcp"
   source_security_group_id = "${aws_security_group.ssh_jump.id}"
 }
-
-#resource "aws_security_group_rule" "stageapp-out-proddb-sql" {
-#  security_group_id = "${aws_security_group.stageapp.id}"
-#  type = "egress"
-#  from_port = 1433
-#  to_port = 1433
-#  protocol = "tcp"
-#  source_security_group_id = "${aws_security_group.proddb.id}"
-#}
 
 resource "aws_security_group_rule" "stageapp-out-proxy" {
   security_group_id = "${aws_security_group.stageapp.id}"
@@ -115,27 +88,20 @@ resource "aws_security_group_rule" "stageapp-out-all" {
   cidr_blocks = ["${var.cidr}","0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "stageapp-out-proddb-psql" {
-  security_group_id        = "${aws_security_group.stageapp.id}"
-  type                     = "egress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.proddb.id}"
-}
+#resource "aws_security_group_rule" "stageapp-out-proddb-psql" {
+#  security_group_id        = "${aws_security_group.stageapp.id}"
+#  type                     = "egress"
+#  from_port                = 5432
+#  to_port                  = 5432
+#  protocol                 = "tcp"
+#  source_security_group_id = "${aws_security_group.proddb.id}"
+#}
 
-resource "aws_security_group" "proddb" {
-    name = "proddb"
-    description = "Allow proddb connections."
-    tags { Name = "proddb" }
-    vpc_id = "${var.vpc_id}"
-}
-
-resource "aws_security_group_rule" "proddb-in-stageapp-psql" {
-  security_group_id        = "${aws_security_group.proddb.id}"
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.stageapp.id}"
-}
+#resource "aws_security_group_rule" "proddb-in-stageapp-psql" {
+#  security_group_id        = "${aws_security_group.proddb.id}"
+#  type                     = "ingress"
+#  from_port                = 5432
+#  to_port                  = 5432
+#  protocol                 = "tcp"
+#  source_security_group_id = "${aws_security_group.stageapp.id}"
+#}
