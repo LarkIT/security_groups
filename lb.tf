d410fd6e8c16c2b22e648d88cee3c411014294bf
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-11-07 08:38:40 -0700

    Adding production lb security group

--- lb.tf
+++ lb.tf
@@ -44,3 +44,51 @@ resource "aws_security_group_rule" "app-lb-out-app-https" {
   protocol                 = "tcp"
   source_security_group_id = "${aws_security_group.stageapp.id}"
 }
+
+
+resource "aws_security_group" "prod-app-lb" {
+    name        = "app-lb"
+    description = "Allow stageapp ELB connections."
+    vpc_id      = "${var.vpc_id}"
+    tags {
+      Name = "prod-app-lb"
+    }
+}
+
+resource "aws_security_group_rule" "prod-app-lb-in-any-http" {
+  security_group_id = "${aws_security_group.prod-app-lb.id}"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "prod-app-lb-in-any-https" {
+  security_group_id = "${aws_security_group.prod-app-lb.id}"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "prod-app-lb-out-app-http" {
+  security_group_id        = "${aws_security_group.prod-app-lb.id}"
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.prodapp.id}"
+}
+
+resource "aws_security_group_rule" "prod-app-lb-out-app-https" {
+  security_group_id        = "${aws_security_group.prod-app-lb.id}"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.prodapp.id}"
+}
