f452d24d2211a070917b77eb1289d0a1dfb5a6b0
Andrew Alles <andrew@lark-it.com>
2017-12-08 14:27:16 -0700

    Disabled psql sg

--- proddb.tf
+++ proddb.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "proddb" {
     vpc_id = "${var.vpc_id}"
 }
 
-resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
-  security_group_id        = "${aws_security_group.proddb.id}"
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.prodapp.id}"
-}
+#resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
+#  security_group_id        = "${aws_security_group.proddb.id}"
+#  type                     = "ingress"
+#  from_port                = 5432
+#  to_port                  = 5432
+#  protocol                 = "tcp"
+#  source_security_group_id = "${aws_security_group.prodapp.id}"
+#}
 
 resource "aws_security_group_rule" "proddb-in-prodapp-mysql" {
   security_group_id        = "${aws_security_group.proddb.id}"
