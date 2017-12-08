de17bd21f1c5d95a4d3e1d209d5e5c4fa976df6c
Andrew Alles <andrew@lark-it.com>
2017-12-08 14:14:53 -0700

    Uncommented proddb for aurora

--- proddb.tf
+++ proddb.tf
@@ -1,16 +1,24 @@
-#resource "aws_security_group" "proddb" {
-#    name = "proddb"
-#    description = "Allow proddb connections."
-#    tags { Name = "proddb" }
-#    vpc_id = "${var.vpc_id}"
-#}
+resource "aws_security_group" "proddb" {
+    name = "proddb"
+    description = "Allow proddb connections."
+    tags { Name = "proddb" }
+    vpc_id = "${var.vpc_id}"
+}
 
-#resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
-#  security_group_id        = "${aws_security_group.proddb.id}"
-#  type                     = "ingress"
-#  from_port                = 5432
-#  to_port                  = 5432
-#  protocol                 = "tcp"
-#  source_security_group_id = "${aws_security_group.prodapp.id}"
-#}
+resource "aws_security_group_rule" "proddb-in-prodapp-psql" {
+  security_group_id        = "${aws_security_group.proddb.id}"
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.prodapp.id}"
+}
 
+resource "aws_security_group_rule" "proddb-in-prodapp-mysql" {
+  security_group_id        = "${aws_security_group.proddb.id}"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.prodapp.id}"
+}
