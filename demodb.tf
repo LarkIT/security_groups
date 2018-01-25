c35b420c3d17e5f99e0c75cb27efd40865462899
Andrew Alles <andrew@lark-it.com>
2018-01-25 11:45:55 -0700

    Added security group to allow mysql for demodb

---
+++ demodb.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "demodb" {
+   name        = "demodb"
+   description = "Allow demodb connections."
+   vpc_id      = "${var.vpc_id}"
+   tags { 
+     Name = "demodb"
+   }
+}
+
+
+resource "aws_security_group_rule" "demodb-in-stageapp-psql" {
+ count                    = "${var.db_type == "postgres" ? 1 : 0 }"
+ security_group_id        = "${aws_security_group.demodb.id}"
+ type                     = "ingress"
+ from_port                = 5432
+ to_port                  = 5432
+ protocol                 = "tcp"
+ source_security_group_id = "${aws_security_group.stageapp.id}"
+}
+
+resource "aws_security_group_rule" "demodb-in-stageapp-mysql" {
+ count                    = "${var.db_type == "mysql" ? 1 : 0 }"
+ security_group_id        = "${aws_security_group.demodb.id}"
+ type                     = "ingress"
+ from_port                = 3306
+ to_port                  = 3306
+ protocol                 = "tcp"
+ source_security_group_id = "${aws_security_group.prodapp.id}"
+}
+
+
+
