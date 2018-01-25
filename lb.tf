c84d62c04371d9ade342b394cb8ec25d2e6d3090
Andrew Alles <andrew@lark-it.com>
2018-01-25 14:10:22 -0700

    Created resources for demo box

--- lb.tf
+++ lb.tf
@@ -64,6 +64,7 @@ resource "aws_security_group" "prod-app-lb" {
     }
 }
 
+
 resource "aws_security_group_rule" "prod-app-lb-in-any-http" {
   security_group_id = "${aws_security_group.prod-app-lb.id}"
   type              = "ingress"
@@ -101,3 +102,58 @@ resource "aws_security_group_rule" "prod-app-lb-out-app-https" {
   protocol                 = "tcp"
   source_security_group_id = "${aws_security_group.prodapp.id}"
 }
+
+
+################################################
+#
+# Demo Security Group
+#
+################################################
+resource "aws_security_group" "demo-app-lb" {
+    name        = "demo-app-lb"
+    description = "Allow prodapp ELB connections."
+    vpc_id      = "${var.vpc_id}"
+    tags {
+      Name = "demo-app-lb"
+    }
+}
+
+
+resource "aws_security_group_rule" "demo-app-lb-in-any-http" {
+  security_group_id = "${aws_security_group.demo-app-lb.id}"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "demo-app-lb-in-any-https" {
+  security_group_id = "${aws_security_group.demo-app-lb.id}"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "demo-app-lb-out-app-http" {
+  security_group_id        = "${aws_security_group.demo-app-lb.id}"
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.demo.id}"
+}
+
+resource "aws_security_group_rule" "demo-app-lb-out-app-https" {
+  security_group_id        = "${aws_security_group.demo-app-lb.id}"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.demo.id}"
+}
+
